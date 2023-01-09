d37fbb2ee0ecbed0db7ba124881e0d4c1f360f99
+ <stingeyandrewstown@live.com>
2023-01-09 10:23:10 -0600

    commit tree 486656d33894a27b7c81d7b6337d5d86523d5640

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 15.5
+let num2 = 4
+let num3 = 10
+let num4 = 6
+let max = 20
 
-// Write your code here
+let multiply = num1 * num2
+let random = Math.floor(Math.random() + 1)
+let mod = num3 % num4
